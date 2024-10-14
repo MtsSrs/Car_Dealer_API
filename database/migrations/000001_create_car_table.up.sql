88820a1e85b30eecbd3347efd34bfb9c3e677ebc
Matheus Soares <matheus.1503@hotmail.com>
2024-10-14 17:46:11 -0300

    Feat: Structure & Migrations

---
+++ database/migrations/000001_create_car_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS cars(
+   car_id serial PRIMARY KEY,
+   car_manufacturer VARCHAR (255) NOT NULL,
+   car_name VARCHAR (255) NOT NULL,
+   car_model VARCHAR (300) NOT NULL,
+   car_year_model VARCHAR(5) NOT NULL
+);
+
+
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mtssrs/car_dealer_API
+
+go 1.23.2
