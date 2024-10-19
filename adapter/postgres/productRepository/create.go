1c77b2c68f1ae440958d3032a6b9dc985aaec1f2
Matheus Soares <matheus.1503@hotmail.com>
2024-10-19 11:07:24 -0300

    feat: Repository Create

---
+++ adapter/postgres/productRepository/create.go
@@ -0,0 +1,36 @@
+package productrepository
+
+import (
+	"context"
+
+	"github.com/mtssrs/car_dealer_API/core/domain"
+	"github.com/mtssrs/car_dealer_API/core/dto"
+)
+
+func (repository repository) Create(carRequest *dto.CreateCarRequest) (*domain.Car, error) {
+	ctx := context.Background()
+	car := domain.Car{}
+
+	err := repository.db.QueryRow(
+		ctx,
+		"INSERT INTO cars (car_manufacturer, car_name, car_model, car_year_model, car_price) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+		carRequest.CarManufacturer,
+		carRequest.CarName,
+		carRequest.CarModel,
+		carRequest.CarYearModel,
+		carRequest.CarPrice,
+	).Scan(
+		&car.CarId,
+		&car.CarManufacturer,
+		&car.CarName,
+		&car.CarModel,
+		&car.CarYearModel,
+		&car.CarPrice,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &car, nil
+}
